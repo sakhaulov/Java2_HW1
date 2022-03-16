09904879f749d88399b11b5924bfd21fb6a303d0
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2022-03-16 22:22:50 +0500

    initial commit

---
+++ src/Course.java
@@ -0,0 +1,32 @@
+public class Course {
+
+    private String name;
+    private Challenge[] course;
+
+    public Course(String name, Challenge[] course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public void doIt(Team team) {
+        Participant[] team_on_course = team.getTeam();
+        for (int i = 0; i < team_on_course.length; i++) {
+            int result = 0;
+            for (int j = 0; j < this.course.length; j++) {
+                if (team_on_course[i].getAbility() >= this.course[j].getDifficulty()) {
+                    result++;
+                    System.out.println("Участник " + team_on_course[i].getName() + " успешно прошёл испытание " + this.course[j].getName());
+                    System.out.println("result = " + result);
+                }
+            }
+            if (result == 3) {
+                team.getTeam_passed().add(team_on_course[i]);
+            }
+
+        }
+
+    }
+
+
+
+}
